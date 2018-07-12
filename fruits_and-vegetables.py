e756c119d41b71c6d1a0c138d9e6b29c09469df7
Rey <ray.greenm@gmail.com>
2018-07-11 21:21:24 -0500

    Rey Green 7_11_18

---
+++ fruits_and-vegetables.py
@@ -0,0 +1,6 @@
+
+
+
+x = ["apple", "banana", "carrot"]
+
+print(x[0]+'s', x[1]+'s', "and", x[2]+'s')
---
+++ naughty_strings.py
@@ -0,0 +1,7 @@
+
+
+string = "This string is made "to show errors" for the homework assignment"
+
+#SyntaxError: invalid syntax
+
+#It is showing an error because the second quotation mark is ending the string which makes the rest of the text confuse the interpeter
